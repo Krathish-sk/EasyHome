5bd6e1b8bbec304fa5314aa46c0e30aed98656c3
Krathish K <krathishs.k123@gmail.com>
2023-10-04 12:17:51 +0530

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About, Home, Profile, SignIn, SignUp } from "./pages";
+
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import Home from "./Home";
+import Profile from "./Profile";
+import About from "./About";
+
+export { SignIn, SignUp, Home, Profile, About };
