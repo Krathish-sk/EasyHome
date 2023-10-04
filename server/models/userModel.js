fc413e524a0c720e3141bca88189b32cdf3965a8
Krathish K <krathishs.k123@gmail.com>
2023-10-04 21:22:33 +0530

    create user model

---
+++ server/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please provide an username"],
+      unique: true,
+      minlength: 3,
+      maxlength: 20,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("User", UserSchema);
+
+export default userModel;
