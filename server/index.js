215b7af773c31561d97779857e40ac033179c9dd
Krathish K <krathishs.k123@gmail.com>
2023-10-05 14:02:53 +0530

    middleware and error handling

--- server/index.js
+++ server/index.js
@@ -6,24 +6,34 @@ import connectDB from "./config/db.js";
 // Connect to Database :-
 connectDB();
 
+// Global routes imports
+import authRoutes from "./routes/authRoutes.js";
+
+// Middleware imports
+import errorHandlerMiddleware from "./middleware/errorHandler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
+
 const port = 5000;
 const app = express();
 app.use(express.json());
 
-// Global routes
-import authRoutes from "./routes/authRoutes.js";
+// Routes
 app.use("/api/auth", authRoutes);
 
+// Middlewares
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
+
 // Error Middleware
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal server error";
-  return res.status(statusCode).json({
-    status: "Failed",
-    statusCode,
-    message,
-  });
-});
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal server error";
+//   return res.status(statusCode).json({
+//     status: "Failed",
+//     statusCode,
+//     message,
+//   });
+// });
 
 app.listen(port, () => {
   console.log("Server running at port ", port);
