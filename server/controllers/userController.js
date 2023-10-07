2aee43a6516fd924110a4af43dc979d9715e9c65
Krathish K <krathishs.k123@gmail.com>
2023-10-07 10:43:10 +0530

    User Controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,81 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/userModel.js";
+import Listing from "../models/listingModel.js";
+import { errorHandler } from "../utils/error.js";
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) return next(errorHandler(404, "User not found !!"));
+
+    const { password: pass, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "Unauthorized"));
+
+  if (
+    !req.body.username ||
+    !req.body.email ||
+    !req.body.photo ||
+    !req.body.number
+  ) {
+    return next(errorHandler(400, "Please provide required values"));
+  }
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          photo: req.body.photo,
+          number: req.body.number,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "Unauthorized"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User have been deleted !");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "Un-Authorized"));
+  }
+  try {
+    const listing = await Listing.find({ userRef: req.params.id });
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
