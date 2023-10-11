a8a234c8494803051e362fc3b32c8ca28ef28cc8
Krathish K <krathishs.k123@gmail.com>
2023-10-11 09:19:52 +0530

    Update Listing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { Header, PrivateRoute } from "./components";
 import "react-toastify/dist/ReactToastify.css";
-import { About, Home, Profile, SignIn, SignUp, CreateListing } from "./pages";
+import {
+  About,
+  Home,
+  Profile,
+  SignIn,
+  SignUp,
+  CreateListing,
+  UpdateListing,
+} from "./pages";
 
 export default function App() {
   return (
@@ -13,11 +21,15 @@ export default function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
-        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,5 +4,6 @@ import Home from "./Home";
 import Profile from "./Profile";
 import About from "./About";
 import CreateListing from "./CreateListing";
+import UpdateListing from "./UpdateListing";
 
-export { SignIn, SignUp, Home, Profile, About, CreateListing };
+export { SignIn, SignUp, Home, Profile, About, CreateListing, UpdateListing };
