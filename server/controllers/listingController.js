ce9381bb1395521fbbe3805a9155fb7921ec17b1
Krathish K <krathishs.k123@gmail.com>
2023-10-11 13:12:21 +0530

    search in header

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -22,7 +22,49 @@ export const getListing = async (req, res, next) => {
   }
 };
 
-export const getListings = async (req, res, next) => {};
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let { offer, furnished, parking, type } = req.query;
+
+    if (offer === false || offer === undefined) {
+      offer = { $in: [false, true] };
+    }
+
+    if (parking === false || parking === undefined) {
+      parking = { $in: [false, true] };
+    }
+
+    if (furnished === false || furnished === undefined) {
+      furnished = { $in: [true, false] };
+    }
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["rent", "sale"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+    const order = req.query.order || "desc";
+    const sort = req.query.sort || "createdAt";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const updateListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
