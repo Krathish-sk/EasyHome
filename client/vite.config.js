21bd484fe9d648477ddf0d0ce4bfee3e9aa32bf1
Krathish K <krathishs.k123@gmail.com>
2023-10-14 11:06:17 +0530

    Update in vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import dns from "dns";
-
-dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:3000",
         secure: false,
       },
     },
-    port: 3000,
   },
+
   plugins: [react()],
 });
